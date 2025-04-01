6040e81605942db024d354d1571bfa3002e1d8a8
Rui Soares <soares.m.rui@gmail.com>
2025-04-01 20:57:59 +0100

    Feito

--- EfolioA/EfolioAv4.py
+++ EfolioA/EfolioAv4.py
@@ -3,18 +3,11 @@ import time
 
 grelha = [
     [
-        [1, 1, 1, 1, 1],
-        [1, 2, 1, 2, 1],
-        [1, 1, 1, 1, 1],
-        [2, 2, -1, 2, 2],
-        [1, 1, 1, 1, 1]
-    ],
-    [
         [1, 1, 1, 1, -1],
         [1, 10, 1, 2, 10],
         [1, -2, 10, 2, 1],
         [10, 10, 1, 1, 1],
-        [-1, 1, 1, 2, -2]
+        [-1, 1, 1, 2, -2],
     ],
     [
         [-1,1,2,2,-1],
@@ -134,7 +127,7 @@ grelha = [
 ]
 
 expansao = 0
-geracao = 1
+geracao = 0
 
 # Define o Estado da Grelha
 class Estado:
@@ -150,20 +143,21 @@ class Estado:
         return self.custo < other.custo
 
 class Problema:
-    def __init__(self, pos_inicial, grelha, tamanho_grelha, funcao_custo):
-        self.pos_inicial = pos_inicial
+    def __init__(self, grelha, tamanho_grelha, funcao_custo, objetivo="Salvar"):
         self.grelha = grelha
         self.tamanho_grelha = tamanho_grelha  # Size of the grid (5x5)
         self.funcao_custo = funcao_custo  # A function to get the cost of an action
+        self.objetivo = objetivo
 
 
     def verifica_objetivo(self, estado):
         if self.grelha[estado.posicao[0]][estado.posicao[1]] < 0:
             return True
 
-    def gerar_sucessores(self, estado):
+    def gerar_sucessores(self, estado, problema, portas, objetivo):
+        global expansao
         # Expand a node to generate child nodes by moving in the grid
-        filhos = []
+        sucessores = []
         acoes_possiveis = [
             (-1, 0),  # cima
             (1, 0),   # baixo
@@ -171,14 +165,37 @@ class Problema:
             (0, 1)    # direita
         ]
 
-        for acao in acoes_possiveis:
-            pos_prox_estado = (estado.posicao[0] + acao[0], estado.posicao[1] + acao[1])
-            # Check if the next state is within grid bounds
-            if 0 <= pos_prox_estado[0] < len(self.grelha) and 0 <= pos_prox_estado[1] < len(self.grelha[0]):
-                custo = self.funcao_custo(estado.posicao, pos_prox_estado)
-                child_node = Estado(posicao=pos_prox_estado, pai=estado, acao=acao, custo=estado.custo + custo, tempo=estado.tempo - custo)
-                filhos.append(child_node)
-        return filhos
+        if expansao == 1:
+            for (sx, sy) in portas:
+                # Instead of modifying the grid, just store the position of 'X'
+                if grelha[sx][sy] != 10:
+                    pos_prox_estado = (sx, sy)
+                    if grelha[sx][sy] == 1:
+                        custo = 1
+                    elif grelha[sx][sy] == 2:
+                        custo = 2
+                    sucessor = Estado(posicao=pos_prox_estado, pai=estado, custo=estado.custo + custo, tempo=estado.tempo - custo)
+                    sucessores.append(sucessor)
+        else:
+            for acao in acoes_possiveis:
+                pos_prox_estado = (estado.posicao[0] + acao[0], estado.posicao[1] + acao[1])
+                # Check if the next state is within grid bounds
+                if 0 <= pos_prox_estado[0] < len(self.grelha) and 0 <= pos_prox_estado[1] < len(self.grelha[0]):
+
+                    if problema.grelha[pos_prox_estado[0]][pos_prox_estado[1]] != 10:
+                        custo = self.funcao_custo(estado.posicao, pos_prox_estado)
+                        if estado.tempo > 0:
+                            sucessor = Estado(posicao=pos_prox_estado, pai=estado, acao=acao, custo=estado.custo + custo, tempo=estado.tempo - custo)
+                            sucessores.append(sucessor)
+
+        #Cortar sucessores k nos afastem da nossa saida
+        if problema.objetivo == "Sair":
+            sucessorProximoSaida = saidaMaisProxima(sucessores, portas)
+            sucessores.clear()
+            sucessores.append(sucessorProximoSaida)
+            return sucessores
+
+        return sucessores
 
     # def __init__(self, pai=None, movimento=None, geracao=0, nivel=0, expansao=0, posicao=None, custo=0, tempototal=0, pessoassalvas = 0, custototal=0):
     #     self.pai = pai
@@ -191,6 +208,26 @@ class Problema:
     #     self.posicao = posicao
     #     self.pessoasSalvas = pessoassalvas
     #     self.tempoTotal = tempototal
+
+def saidaMaisProxima(coordinates, portas):
+    # List of possible exits
+    #saidas = [(0, 2), (2, 0), (4, 2), (2, 4)]
+
+    # Initialize the minimum distance as a large number
+    min_distance = float('inf')
+    closest_coordinate = None
+
+    # Loop through all coordinates and find the closest one
+    for coord in coordinates:
+        # Check distance to each exit and pick the closest one
+        for saida in portas:
+            distance = manhattan_distance(coord, saida)
+            if distance < min_distance:
+                min_distance = distance
+                closest_coordinate = coord
+                #print(f"GGGGGGGGGGGGGGGGGGGGGGGGG         {closest_coordinate} ")
+
+    return closest_coordinate
 
 def imprimeGrelha(estado, grelha):
     transformaGrelha = []  # To store the new transformed grid
@@ -273,12 +310,12 @@ def imprimeGrelha2(caminho, grelha):
 
 def refazerCaminho(estado):
     caminho = []
-    while estado:
+
+    while estado is not None:
         caminho.append(estado.posicao)
         estado = estado.pai
-    caminho = caminho[::-1]  # Reverse the path to show from start to goal
 
-    return caminho
+    return caminho[::-1]  # Reverse the path to get it from the start to the goal
 
 def manhattan_distance(start, exit):
     # Calculate Manhattan distance between start and exit
@@ -315,7 +352,7 @@ def gerarSucessoresIniciais(grelha, portas):
 
     return successors
 
-def gerarSucessores(grelha, portas, estadoAtual, k, objetivo):
+def gerarSucessores(grelha, portas, estadoAtual, k, objetivo, ):
     # Define the possible movement directions: up, down, left, right
     directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]  # (dy, dx) for up, down, left, right
 
@@ -380,20 +417,23 @@ def custo_movimento(estado, prox_estado):
     else:
         return 1
 
-def custo_uniforme(problema, tempo, k):
+def custo_uniforme(problema, tempo, k, portas):
     global expansao
     global geracao
+
     # Create initial node
-    estado = Estado(posicao=problema.pos_inicial, custo=1, tempo=tempo - 1)
+    estado = Estado(posicao=None, custo=1, tempo=tempo)
 
     # Priority Queue (Min-Heap)
     fronteira = []
     heapq.heappush(fronteira, estado)
 
 
+    solucao = []
+    encontrados = set()
+    salvos = 0
     # Set of reached states to avoid revisiting
     visitado = set()
-    visitado.add(problema.pos_inicial)
 
     while fronteira:
         # Pop the node with the least cost
@@ -401,12 +441,35 @@ def custo_uniforme(problema, tempo, k):
         expansao += 1
 
         # If it's a goal node, return it
-        if problema.verifica_objetivo(estado):
+        if problema.objetivo != "Sair":
+            if expansao != 1 and problema.verifica_objetivo(estado) and estado.posicao not in encontrados:
+                salvos += 1
 
-            return estado
+                encontrados.add(estado.posicao)
+                sx, sy = estado.posicao
+                estado.tempo += abs(problema.grelha[sx][sy])
+                fronteira.clear()
+                visitado.clear()
+
+                if salvos < k:
+                    heapq.heappush(fronteira, estado)
+                    solucao.append(estado)
+
+                # if k == salvos:
+                #     return solucao
+        #if estado.tempo <= int(len(problema.grelha) / 2) and estado.posicao in portas and salvos == k:
+        if  estado.posicao in portas and salvos == k:
+            problema.objetivo = "Sair"
+            estado.custo += 1
+            solucao.append(estado)
+
+            # caminho = refazerCaminho(estado)
+            # print(caminho)
+            # #print("Fim")
+            return solucao
 
         # Expand the node and add children to the frontier
-        for filho in problema.gerar_sucessores(estado):
+        for filho in problema.gerar_sucessores(estado, problema, portas, problema.objetivo):
             if filho.posicao not in visitado:
                 visitado.add(filho.posicao)
                 heapq.heappush(fronteira, filho)
@@ -414,20 +477,21 @@ def custo_uniforme(problema, tempo, k):
 
     return None
 
-
-def get_exits(grid):
-    # Calculate grid size
-    rows = len(grid)
-    cols = len(grid[0]) if rows > 0 else 0
-
-    # List of relative exit positions (fixed positions based on a smaller grid)
-    exits = [(0, cols // 2), (rows - 1, cols // 2), (rows // 2, 0), (rows // 2, cols - 1)]
-
-    return exits
-
 def tamanhoGrelha(grid):
     result = len(grid)        # Number of rows
     return (result, result)
+
+def calcular_portas(grelha):
+    # Calculate grid size
+    rows = len(grelha)
+    cols = len(grelha) if rows > 0 else 0
+
+    # List of relative exit positions (fixed positions based on a smaller grid)
+    portas = [(0, cols // 2), (rows - 1, cols // 2), (rows // 2, 0), (rows // 2, cols - 1)]
+
+    return portas
+
+
 
 # portas
 #portas = [(0, 2), (4, 2), (2, 0), (2, 4)]
@@ -444,51 +508,96 @@ grid_choice = int(input("Enter the number of the grid you want to use: "))
 
 # Switch logic
 if grid_choice == 1:
-    grelha = grelha[grid_choice - 1]
-    print("You have selected Grid 1")
+    grelha = grelha[0]
     tamanho_grelha = tamanhoGrelha(grelha[0])
-    print(tamanho_grelha)
+    portas = calcular_portas(grelha[0])
     tempo = 10
     k = 2
 elif grid_choice == 2:
-    grelha = grelha[grid_choice - 1]
-    print("You have selected Grid 2")
-    tamanho_grelha = tamanhoGrelha(grelha[0])
-    print(tamanho_grelha)
+    grelha = grelha[1]
+    tamanho_grelha = tamanhoGrelha(grelha[1])
+    portas = calcular_portas(grelha[1])
+    tempo = 20
     k = 3
+elif grid_choice == 3:
+    grelha = grelha[2]
+    tamanho_grelha = tamanhoGrelha(grelha[2])
+    portas = calcular_portas(grelha[2])
+    tempo = 15
+    k = 3
+elif grid_choice == 4:
+    grelha = grelha[3]
+    tamanho_grelha = tamanhoGrelha(grelha[3])
+    portas = calcular_portas(grelha[3])
+    tempo = 20
+    k = 4
+elif grid_choice == 5:
+    grelha = grelha[4]
+    tamanho_grelha = tamanhoGrelha(grelha[4])
+    portas = calcular_portas(grelha[4])
+    tempo = 19
+    k = 4
+elif grid_choice == 6:
+    grelha = grelha[5]
+    tamanho_grelha = tamanhoGrelha(grelha[5])
+    portas = calcular_portas(grelha[5])
+    tempo = 48
+    k = 6
+elif grid_choice == 7:
+    grelha = grelha[6]
+    tamanho_grelha = tamanhoGrelha(grelha[6])
+    portas = calcular_portas(grelha[6])
+    tempo = 120
+    k = 10
+elif grid_choice == 8:
+    grelha = grelha[7]
+    tamanho_grelha = tamanhoGrelha(grelha[7])
+    portas = calcular_portas(grelha[7])
+    tempo = 30
+    k = 10
+elif grid_choice == 9:
+    grelha = grelha[8]
+    tamanho_grelha = tamanhoGrelha(grelha[8])
+    portas = calcular_portas(grelha[8])
+    tempo = 45
+    k = 10
+elif grid_choice == 10:
+    grelha = grelha[9]
+    tamanho_grelha = tamanhoGrelha(grelha[9])
+    portas = calcular_portas(grelha[9])
+    tempo = 45
+    k = 15
+else:
+    print("Escolha invalida!")
+    exit()
 
 start_time = time.time()
 
 # solucaoFinal = bfs(grelha, estadoInicial, portas, k)
 # Create a problem instance
-problema = Problema(pos_inicial=(2,0), grelha=grelha, tamanho_grelha=tamanho_grelha, funcao_custo=custo_movimento)
+problema = Problema(grelha=grelha, tamanho_grelha=tamanho_grelha, funcao_custo=custo_movimento)
 
 # Run the Uniform-Cost Search
-resultado = custo_uniforme(problema, tempo, k)
+resultado = custo_uniforme(problema, tempo, k, portas)
 
 # Print the result path
 if resultado:
-    caminho = refazerCaminho(resultado)
-    print(caminho)
-    imprimeGrelha2(caminho, grelha)
-    #print(f"Tempo: {resultado.custo} ({index + 1}/{total}), custo: {item.custoTotal}")
-    print(f"Tempo: {resultado.tempo}, custo: {resultado.custo}")
-    print(f"Expansoes: {expansao}")
-    print(f"Geracoes: {geracao}")
+    for index, item in enumerate(resultado):
+        print(f"Parte {index + 1} passos ")
+        caminho = refazerCaminho(item)
+        print(caminho)
+        imprimeGrelha2(caminho, grelha)
+
+        print(f"Tempo: {item.tempo} ({index + 1}/{len(resultado)}), custo: {item.custo}")
+        print(f"Expansoes: {expansao}")
+        print(f"Geracoes: {geracao}")
 else:
     print("No solution found")
 
-
-# class Problema:
-#     def __init__(self, estado_inicial, objetivo, tamanho_grelha, funcao_custo):
-#         self.estado_inicial = estado_inicial
-#         self.objetivo = objetivo
-#         self.tamanho_grelha = tamanho_grelha  # Size of the grid (5x5)
-#         self.funcao_custo = funcao_custo  # A function to get the cost of an action
-
-
 end_time = time.time()
 elapsed_time = end_time - start_time
+
+print(f"\nTime taken to find the solution: {elapsed_time:.4f} seconds")
 
 #     #estadoInicial = EstadoGrelha(geracao=geracao, tempototal=10)
 #     #imprimeGrelha(estadoInicial, grelha)
